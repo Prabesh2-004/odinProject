f3f79895b6bbe2b48de5a43fc215a78f3d126675
Prabesh2-004 <coder.bro2004@gmail.com>
2025-04-25 17:31:25 +0545

    Some changes

---
+++ Scripts/rockpaperscissor.js
@@ -0,0 +1,35 @@
+function humanMove() {
+let Person = prompt("Enter Your Move: ");
+let moves;
+if(Person){
+    moves = Person.toLowerCase();
+    return moves;
+}
+return moves;
+}
+function computerMove(){
+const Moves = ['rock', 'paper', 'scissor'];
+const randomMove = Math.floor(Math.random() * Moves.length);
+const computerMoves = Moves[randomMove].toLowerCase();
+return computerMoves;
+}
+let result;
+const computerMoves = computerMove();
+const humanMoves = humanMove();
+function play(computerMove, humanMove){
+    if(humanMove === computerMove){
+        result = 'Tie';
+    }
+    else if(humanMove === 'rock' && computerMove === 'paper'|| humanMove === 'scissor' && computerMove === 'rock'|| humanMove === 'paper' && computerMove === 'scissor'){
+        result = 'Lost'
+    }
+    else if (humanMove === 'rock' && computerMove === 'scissor'|| humanMove === 'scissor' && computerMove === 'paper'|| humanMove === 'paper' && computerMove === 'rock'){
+        result = 'Win';
+    }
+    else{
+        console.log('Invalid Choice!');
+        return;
+    }
+    console.log(`Computer: ${computerMoves} and Human: ${humanMoves} Result: ${result}`);
+}
+play(computerMoves, humanMoves);
---
+++ Scripts/sketch.js
@@ -0,0 +1,38 @@
+const display = document.querySelector('.container');
+const inputBox = document.querySelector('.input-box');
+const addButton = document.querySelector('.addBtn');
+const resetContainer = document.querySelector('.reset-container');
+const divdiv = '<div class="div-box"></div>';
+
+addButton.addEventListener('click', function () {
+    const inputElement = parseInt(inputBox.value);
+    if (inputElement > 0 && inputElement <= 100) {
+        let multipleDiv = '';
+        for (let i = 0; i < inputElement * inputElement; i++) {
+            multipleDiv += divdiv;
+        }
+        display.innerHTML = multipleDiv;
+
+        Object.assign(display.style, {
+            display: 'grid',
+            gridTemplateColumns: `repeat(${inputElement}, 1fr)`,
+            gridTemplateRows: `repeat(${inputElement}, 1fr)`,
+        });
+        const divBoxes = document.querySelectorAll('.div-box');
+        divBoxes.forEach(div => {
+            div.addEventListener('mouseover', function () {
+                this.style.backgroundColor = 'black';
+            });
+        });
+
+        inputBox.value = "";
+    } else {
+        alert("Number must be between 1 to 100");
+        inputBox.value = "";
+    }
+});
+
+resetContainer.addEventListener('click', () => {
+    display.innerHTML = '';
+})
+
